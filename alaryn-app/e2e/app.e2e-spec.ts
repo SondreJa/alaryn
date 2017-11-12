dd2e91e42ad22c1b56351aa1b5634745b79ddca4
Mjupi <mjupithemop@gmail.com>
2017-11-12 18:01:49 +0100

    Using prebuild to learn

--- alaryn-app/e2e/app.e2e-spec.ts
+++ alaryn-app/e2e/app.e2e-spec.ts
@@ -1,14 +1,14 @@
-import { AppPage } from './app.po';
+import { AngularSamplePage } from './app.po';
 
-describe('alaryn-app App', () => {
-  let page: AppPage;
+describe('angular-sample App', () => {
+  let page: AngularSamplePage;
 
   beforeEach(() => {
-    page = new AppPage();
+    page = new AngularSamplePage();
   });
 
-  it('should display welcome message', () => {
+  it('should display message saying app works', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual('app works!');
   });
 });
